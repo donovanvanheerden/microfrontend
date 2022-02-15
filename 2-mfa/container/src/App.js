19d1b23863646f7ec8ea7d42b416bc162e18c532
Donovan van Heerden <dp.vanheerden@gmail.com>
2022-02-15 08:49:25 +0200

    updated how classnames are generated

--- 2-mfa/container/src/App.js
+++ 2-mfa/container/src/App.js
@@ -1,14 +1,21 @@
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+})
+
 export default () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
