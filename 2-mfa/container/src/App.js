c398b10a544c77613cd2e6436d99ab07afe234a3
Donovan van Heerden <dp.vanheerden@gmail.com>
2022-02-15 09:41:25 +0200

    sync history between marketing and container

--- 2-mfa/container/src/App.js
+++ 2-mfa/container/src/App.js
@@ -1,6 +1,6 @@
 import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
--- 2-mfa/marketing/src/bootstrap.js
+++ 2-mfa/marketing/src/bootstrap.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 import App from './App';
 
-const mount = (element) => {
-    ReactDOM.render(<App />, element);
+const mount = (element, { defaultHistory, onNavigate }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) history.listen(onNavigate);
+
+    ReactDOM.render(<App history={history} />, element);
+
+    return {
+      onParentNavigate({ pathname: nextPathname }) {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) history.push(nextPathname);
+      }
+    }
 }
 
 if (process.env.NODE_ENV === 'development') {
     const element = document.querySelector('#_marketing');
 
-    if (element) mount(element);
+    if (element) {
+      mount(element, { defaultHistory: createBrowserHistory() });
+    }
 }
 
 export { mount };
