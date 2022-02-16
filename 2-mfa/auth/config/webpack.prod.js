093eb16e78b22442b7e7511bbb49f73b2b9d51cc
Donovan van Heerden <dp.vanheerden@gmail.com>
2022-02-16 09:53:57 +0200

    added auth frontend

---
+++ 2-mfa/auth/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const packageJson = require('../package.json');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/auth/latest/'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'auth',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './AuthApp': './src/bootstrap'
+      },
+      shared: packageJson.dependencies,
+    }),
+  ]
+});
---
+++ 2-mfa/auth/src/bootstrap.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
+import App from './App';
+
+const mount = (element, { defaultHistory, initialPathname: initialPath, onNavigate }) => {
+    const history = defaultHistory || createMemoryHistory({
+      initialEntries: [initialPath]
+    });
+
+    if (onNavigate) history.listen(onNavigate);
+
+    ReactDOM.render(<App history={history} />, element);
+
+    return {
+      onParentNavigate({ pathname: nextPathname }) {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) history.push(nextPathname);
+      }
+    }
+}
+
+if (process.env.NODE_ENV === 'development') {
+    const element = document.querySelector('#_auth');
+
+    if (element) {
+      mount(element, { defaultHistory: createBrowserHistory() });
+    }
+}
+
+export { mount };
